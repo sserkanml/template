0220b7000334d92bb6466f249cfedb7b1ab472d5
Serkan <snamli828@gmail.com>
2023-02-20 00:47:07 +0300

    template

---
+++ birbahce.com.tr/Scripts/Form.js
@@ -0,0 +1,207 @@
+﻿function SendForm() {
+    var name = $("input[name=NameSurname]").val();
+    var phone = $("input[name=Phone]").val();
+    var email = $("input[name=Email]").val();
+    var errorName = $("#errorName");
+    var errorPhone = $("#errorPhone");
+    var errorEmail = $("#errorEmail");
+    var errorMessage = $("#errorMessage");
+    var permit = false;
+    var permit2 = false;
+
+    if ($('#permitStatus').prop('checked')) {
+        permit = true;
+    }
+    if ($('#permitStatus_2').prop('checked')) {
+        permit2 = true;
+    }
+
+    var ip = "::1";
+    try {
+        ip = myip;
+    }
+    catch (err) {
+        //document.getElementById("demo").innerHTML = err.message;
+    }
+    var utm_Source = $.QueryString("utm_source"),
+        utm_Source = (!utm_Source) ? "null" : utm_Source; // Passing the value null to string
+
+    var utm_Medium = $.QueryString("utm_medium"),
+        utm_Medium = (!utm_Medium) ? "null" : utm_Medium;
+
+    var utm_Term = $.QueryString("utm_term"),
+        utm_Term = (!utm_Term) ? "null" : utm_Term;
+
+    var utm_Content = $.QueryString("utm_content"),
+        utm_Content = (!utm_Content) ? "null" : utm_Content;
+
+    var utm_Campaign = $.QueryString("utm_campaign"),
+        utm_Campaign = (!utm_Campaign) ? "null" : utm_Campaign;
+
+    //$(".input-group input").removeClass('alert');
+    //$(".input-group texarea").removeClass('alert');
+    if (name === null || name === "" || name.length < 2) {
+        $("input[name=NameSurname]").addClass('alert');
+        $(errorMessage).html("Lütfen Adınızı ve Soyadınızı Giriniz!")
+    }
+    else if (!$.isNumeric(phone)) {
+        $("input[name=Phone]").addClass('alert');
+        $(errorMessage).html("Lütfen Telefon Numarası Giriniz!");
+        return false;
+    }
+    else if (phone === null || phone === "" || phone.length < 10 ) {
+        $("input[name=Phone]").addClass('alert');
+        $(errorMessage).html("Lütfen Telefon Numarası Giriniz!");
+    }
+    else if (!validateEmail(email)) {
+        $("input[name=Email]").addClass('alert');
+        $(errorMessage).html("Lütfen E-Posta Adresinizi Giriniz!");
+    }
+    else if ($("#permitStatus_2").prop('checked') == false) {
+        $(errorMessage).html("* Lütfen Metni Okuyup Onaylayınız!");
+    }
+    
+    else {
+        $("#btnSendForm").hide();
+        $(errorMessage).html("")
+        setTimeout(function () {
+            $(".loading").fadeIn(300);
+        }, 50);
+        $("#btnSendForm").attr("type", "submit");
+
+    };
+}
+function SendIFrameForm() {
+    var name = $("input[name=NameSurname]").val();
+    var phone = $("input[name=Phone]").val();
+    var email = $("input[name=Email]").val();
+    var errorName = $("#errorName");
+    var errorPhone = $("#errorPhone");
+    var errorEmail = $("#errorEmail");
+    var permit = false;
+    var permit2 = false;
+
+    if ($('#permitStatus').prop('checked')) {
+        permit = true;
+    }
+    if ($('#permitStatus_2').prop('checked')) {
+        permit2 = true;
+    }
+
+    var ip = "::1";
+    try {
+        ip = myip;
+    }
+    catch (err) {
+        //document.getElementById("demo").innerHTML = err.message;
+    }
+   
+
+    //$(".input-group input").removeClass('alert');
+    //$(".input-group texarea").removeClass('alert');
+    if (name === null || name === "" || name.length < 2) {
+        $("input[name=NameSurname]").addClass('alert');
+        $(errorMessage).html("Lütfen Adınızı ve Soyadınızı Giriniz!")
+    }
+    else if (!$.isNumeric(phone)) {
+        $("input[name=Phone]").addClass('alert');
+        $(errorMessage).html("Lütfen Telefon Numarası Giriniz!");
+        return false;
+    }
+    else if (phone === null || phone === "" || phone.length < 10) {
+        $("input[name=Phone]").addClass('alert');
+        $(errorMessage).html("Lütfen Telefon Numarası Giriniz!");
+    }
+    else if (!validateEmail(email)) {
+        $("input[name=Email]").addClass('alert');
+        $(errorMessage).html("Lütfen E-Posta Adresinizi Giriniz!");
+    }
+    else if ($("#permitStatus_2").prop('checked') == false) {
+        $(errorMessage).html("* Lütfen Metni Okuyup Onaylayınız!");
+    }
+
+    else {
+        $("#btnSendForm").hide();
+        $(errorMessage).html("")
+        setTimeout(function () {
+            $(".loading").fadeIn(300);
+        }, 50);
+        $("#btnSendForm").attr("type", "submit");
+      
+    };
+}
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+
+
+
+var namePart = $("input[name=NameSurname]");
+var phoneNumPart = $("input[name=Phone]");
+var emailPart = $("input[name=Email]");
+var partName = "";
+
+function partValidate(part, errorPartClass) {
+    var newErrorPart = $("#error" + errorPartClass)
+    //console.log(part)
+    if (part[0].name == "NameSurname") {
+        if (part[0].value === null || part[0].value === "") {
+            $(part).addClass('alert');
+            $(newErrorPart).html("Lütfen Adınızı ve Soyadınızı Giriniz!")
+        }
+        else {
+            $(part).removeClass('alert');
+            $(newErrorPart).html("");
+        }
+    }
+    if (part[0].name == "Phone") {
+        $(part[0]).val($(part[0]).val().replace(/[^\d].+/, ""));
+        if ((event.which < 48 || event.which > 57)) {
+            event.preventDefault();
+        }
+        if (part[0].value === null || part[0].value === "" || part[0].value.length < 10) {
+            $(part).addClass('alert');
+            $(newErrorPart).html("Lütfen en az 10 Haneli Telefon Numarası Giriniz!")
+        }
+        else {
+            $(part).removeClass('alert');
+            $(newErrorPart).html("");
+        }
+    }
+
+    if (part[0].name == "Email") {
+        if (!validateEmail(part[0].value)) {
+            $(part).addClass('alert');
+            $(newErrorPart).html("Lütfen E-Posta Adresinizi Giriniz!");
+        }
+        else {
+            $(part).removeClass('alert');
+            $(newErrorPart).html("");
+        }
+    }
+
+}
+
+$("form input").on("keypress keyup blur", function () {
+    var $this = $(this);
+    //console.log($this[0].name);
+    if ($this[0].name == namePart[0].name) {
+        partName = "Name";
+    } else if ($this[0].name == phoneNumPart[0].name) {
+        partName = "Phone"
+    } else if ($this[0].name == emailPart[0].name) {
+        partName = "Email"
+    }
+
+    partValidate($this, partName)
+})
+
+$('.formArea .form .nameSurname').bind('keyup blur keypress', function () {
+    var $this = $(this);
+    $this.val($this.val().replace(/[^a-zŞşÜüİiIıÇçÖöĞğ ]/g, ''));
+    if ($this.val().match(/[^a-zA-ZŞşÜüİiIıÇçÖöĞğ ]/g)) {
+        $this.val(val.replace(/[^a-zA-ZŞşÜüİiIıÇçÖöĞğ ]/g, ''));
+    }
+});
---
+++ birbahce.com.tr/Scripts/main.js
@@ -0,0 +1,255 @@
+﻿$(document).ready(function () {
+    setTimeout(function () {
+        $(".loading").fadeOut();
+    }, 150);
+
+    if (window.location.href.indexOf("#onlinekatalog") > -1) {
+        $("#print").trigger('click');
+    }
+
+    formOpen();
+    formMobile();
+    menuActive();
+    galleryPageSlider();
+
+
+    $('.sliderArea').slick({
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        navigation: false,
+        fade: true,
+        autoplay: true,
+        playspeed: 5000,
+        dots: true,
+        navigation: true
+    });
+  
+});
+
+
+function formOpen() {
+    $(".formArea .formBtnArea").click(function () {
+        $(".formArea").toggleClass("open")
+        $(".formArea .form").removeClass("scroll")
+    })
+}
+
+$(".homePage .bannerArea .arrowArea .arrow").click(function () {
+    $("html, body").animate({ scrollTop: $(".homePage .section-1").offset().top - 20 }, 1000);
+})
+
+
+/*menu active*/
+function menuActive() {
+    var pathName = location.pathname;
+
+    $('header .menuArea ul li .link[href*="' + pathName + '"]').each(function () {
+        $(this).addClass("active");
+        if ((window.location.href.indexOf("/#") > -1) || pathName == '/') {
+            $("header .menuArea ul li .link").removeClass("active");
+        }
+    });
+
+}
+
+
+
+//$(function () {
+//    $(".floorPlan .tab-content").removeClass("active");
+//    $(".floorPlan .tab-content").first().addClass("active");
+
+//    $(".floorPlan .tab-content .tab-content-inside .tab-pane").removeClass("active");
+//    $(".floorPlan .tab-content.active .tab-content-inside .tab-pane").first().addClass("active");
+
+//    $(".floorPlan .nav-tabs li").on("click", function () {
+
+//        var order = $(this).index();
+//        $(".floorPlan .tab-content").removeClass("active");
+//        $(".floorPlan .tab-content").eq(order).addClass("active");
+
+//        $(".floorPlan .tab-content .tab-content-inside .tab-pane").removeClass("active");
+//        $(".floorPlan .tab-content.active .tab-content-inside .tab-pane").first().addClass("active");
+
+//        $(".floorPlan .tab-content .tab-pane .nav-tabs-left li").removeClass("active");
+//        $(".floorPlan .tab-content.active .tab-pane .nav-tabs-left li").first().addClass("active");
+
+//        $(".floorPlan .nav-tabs li").removeClass("active");
+//        $(this).addClass("active");
+//    });
+
+
+
+
+//    $(".floorPlan .tab-pane .nav-tabs-left li").on("click", function () {
+//        var order = $(this).index();
+//        $(this).parent().parent().find(".tab-content-inside .tab-pane").removeClass("active");
+//        $(this).parent().parent().find(".tab-content-inside .tab-pane").eq(order).addClass("active");
+
+//        $(".floorPlan .tab-pane .nav-tabs-left li").removeClass("active");
+//        $(this).addClass("active");
+//    });
+//});
+
+$(function () {
+    $(".tab-content").removeClass("active");
+    $(".tab-content").first().addClass("active");
+
+    $(".tab-content .tab-content-inside .tab-pane").removeClass("active");
+    $(".tab-content.active .tab-content-inside .tab-pane").first().addClass("active");
+
+    $(".nav-tabs li").on("click", function () {
+
+        var order = $(this).index();
+        $(".tab-content").removeClass("active");
+        $(".tab-content").eq(order).addClass("active");
+
+        $(".tab-content .tab-content-inside .tab-pane").removeClass("active");
+        $(".tab-content.active .tab-content-inside .tab-pane").first().addClass("active");
+
+        $(".tab-content .tab-pane .nav-tabs-left li").removeClass("active");
+        $(".tab-content.active .tab-pane .nav-tabs-left li").first().addClass("active");
+
+        $(".nav-tabs li").removeClass("active");
+        $(this).addClass("active");
+
+    });
+
+
+    $(".tab-pane .nav-tabs-left li").on("click", function () {
+        var order = $(this).index();
+        $(this).parent().parent().find(".tab-content-inside .tab-pane").removeClass("active");
+        $(this).parent().parent().find(".tab-content-inside .tab-pane").eq(order).addClass("active");
+
+        $(".tab-pane .nav-tabs-left li").removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+$(".nav-tabs li").click(function () {
+
+    if ($(window).width() < 768 && $(".tab-content.active .tab-pane .nav-tabs-left").length) {
+        $("html, body").animate({ scrollTop: $(".tab-content.active .tab-pane .nav-tabs-left").offset().top }, 1000);
+    } else {
+        $("html, body").animate({ scrollTop: $(".mainTitle").offset().top }, 1000);
+    }
+})
+
+$(".nav-tabs-left li").click(function () {
+
+    if ($(window).width() < 768) {
+        $("html, body").animate({ scrollTop: $(".tab-content.active .tab-pane .nav-tabs-left").offset().top }, 1000);
+    } else {
+        $("html, body").animate({ scrollTop: $(".mainTitle").offset().top }, 1000);
+    }
+})
+
+
+$("#mobileMenuButton").click(function () {
+    $("#mobileMenuButton .wrap").toggleClass("change");
+    $("header .menuArea").slideToggle();
+    if ($("#mobileMenuButton .wrap").hasClass("change")) {
+        $("body").css({ "overflow-y": "hidden" })
+    } else {
+        $("body").css({ "overflow-y": "auto" })
+    }
+})
+
+$("#backToTop").click(function () {
+    $("html, body").animate({ scrollTop: $("body").offset().top }, 1000);
+})
+
+function formMobile() {
+    if ($(window).width() < 768) {
+        $(".formArea").removeClass("open")
+    }
+}
+
+
+
+$(window).scroll(function () {
+    if ($(window).width() > 768 && $(window).scrollTop() > 50) {
+        $(".formArea").removeClass("open")
+    }
+});
+
+$(".formArea .form input").click(function () {
+    if ($(window).width() < 768) {
+        $(".formArea .form").addClass("scroll")
+    }
+})
+
+$(".formArea .form textarea.message").focusin(function () {
+    $(".textsArea .label").hide();
+})
+
+
+
+$(".formArea .form textarea.message").on('keypress keyup blur', function (e) {
+    var val = $(this).val();
+    if (val.length == 0) {
+        $(".textsArea .label").show();
+    } else {
+        $(".textsArea .label").hide();
+    }
+});
+
+/*galleryPage*/
+function galleryPageSlider() {
+    $('.slider')
+        .on('init', function () {
+            //$(".slick-center").prev().removeClass('nextdiv').addClass('prevdiv');
+            //$(".slick-center").next().removeClass('prevdiv').addClass('nextdiv');
+
+            $(".slick-center").next().css({ "z-index": "2", "transform": "scale(.9)" });
+            $(".slick-center").css({ "z-index": "3", "transform": "scale(1)" })
+            $(".slick-center").prev().css({ "z-index": "2", "transform": "scale(.9)" });
+
+
+        })
+        .slick({
+            centerMode: true,
+            focusOnSelect: true,
+            centerPadding: '0px',
+            slidesToShow: 5,
+            cssEase: 'ease',
+            easing: 'linear',
+            swipe: true,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: true,
+                        centerMode: true,
+                        centerPadding: '0px',
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: true,
+                        centerMode: true,
+                        centerPadding: '0px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        }).on('afterChange', function () {
+            //$(".slick-slide").removeClass("nextdiv").removeClass("prevdiv");
+            //$(".slick-center ").prev().removeClass('nextdiv').addClass('prevdiv')
+            //$(".slick-center ").next().removeClass('prevdiv').addClass('nextdiv')
+
+    $('.slider')
+            $(".slider .slick-slide").css({ "z-index": "1", "transform": "scale(.7)" })
+            $(".slick-center").next().css({ "z-index": "2", "transform": "scale(.9)" });
+            $(".slick-center").css({ "z-index": "3", "transform": "scale(1)" })
+            $(".slick-center").prev().css({ "z-index": "2", "transform": "scale(.9)" });
+            
+
+        });
+
+
+}
